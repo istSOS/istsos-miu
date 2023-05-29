c42868705be9eb3b5a79fb1fb061519787f8ff8a
filippofinke <filippofinke@protonmail.ch>
2023-05-29 14:24:32 +0200

    feat: add support for expand keyword

--- src/sta2rest/sta2rest.py
+++ src/sta2rest/sta2rest.py
@@ -7,86 +7,148 @@ This module provides utility functions to convert various elements used in Senso
 representations in a REST API.
 """
 class STA2REST:
+    ENTITY_MAPPING = {
+        "Things": "Thing",
+        "Locations": "Location",
+        "Sensors": "Sensor",
+        "ObservedProperties": "ObservedProperty",
+        "Datastreams": "Datastream",
+        "Observations": "Observation",
+        "FeaturesOfInterest": "FeatureOfInterest",
+        "HistoricalLocations": "HistoricalLocation",
+    }
+
+    PROPERTY_MAPPING = {
+        "name": "name",
+        "description": "description",
+        "encodingType": "encodingType",
+        "metadata": "metadata",
+        "definition": "definition",
+    }
+
+    QUERY_PARAM_MAPPING = {
+        "$filter": "filter",
+        "$orderby": "order",
+        "$top": "limit",
+        "$skip": "offset",
+        "$select": "select",
+        "$count": "count",
+        "$expand": "expand",
+    }
+
     @staticmethod
     def convert_entity(entity: str) -> str:
-        """
-        Convert an entity name to its corresponding SensorThings entity representation.
-
-        Args:
-            entity (str): The entity name to convert.
-
-        Returns:
-            str: The corresponding SensorThings entity representation.
-        """
-        entity_mapping = {
-            "Things": "sensorthings.\"Thing\"",
-            "Locations": "sensorthings.\"Location\"",
-            "Sensors": "sensorthings.\"Sensor\"",
-            "ObservedProperties": "sensorthings.\"ObservedProperty\"",
-            "Datastreams": "sensorthings.\"Datastream\"",
-            "Observations": "sensorthings.\"Observation\"",
-            "FeaturesOfInterest": "sensorthings.\"FeatureOfInterest\"",
-            "HistoricalLocations": "sensorthings.\"HistoricalLocation\"",
-        }
-        return entity_mapping.get(entity, entity)
+        return STA2REST.ENTITY_MAPPING.get(entity, entity)
 
     @staticmethod
     def convert_property(property_name: str) -> str:
-        """
-        Convert a property name to its corresponding SensorThings property representation.
-
-        Args:
-            property_name (str): The property name to convert.
-
-        Returns:
-            str: The corresponding SensorThings property representation.
-        """
-        property_mapping = {
-            "name": "\"name\"",
-            "description": "\"description\"",
-            "encodingType": "\"encodingType\"",
-            "metadata": "\"metadata\"",
-            "definition": "\"definition\"",
-        }
-        return property_mapping.get(property_name, property_name)
+        return STA2REST.PROPERTY_MAPPING.get(property_name, property_name)
 
     @staticmethod
     def convert_query_param(query_param: str) -> str:
-        """
-        Convert a query parameter to its corresponding SensorThings query parameter representation.
+        return STA2REST.QUERY_PARAM_MAPPING.get(query_param, query_param)
 
-        Args:
-            query_param (str): The query parameter to convert.
+    @staticmethod
+    def split_but_not_between(query: str, split: str, delim1: str, delim2: str) -> list:
+        result = []
+        stack = []
+        current = ''
+        for char in query:
+            if char == delim1:
+                current += char
+                stack.append(delim1)
+            elif char == delim2:
+                current += char
+                if stack and stack[-1] == delim1:
+                    stack.pop()
+            elif char == split:
+                if stack:
+                    # Inside parentheses, treat comma as part of the current substring
+                    current += char
+                else:
+                    # Outside parentheses, split the string and reset the current substring
+                    result.append(current.strip())
+                    current = ''
+            else:
+                # Regular character, add it to the current substring
+                current += char
+        
+        if current:
+            result.append(current.strip())
 
-        Returns:
-            str: The corresponding SensorThings query parameter representation.
-        """
-        query_param_mapping = {
-            "filter": "filter",
-            "orderby": "order",
-            "top": "limit",
-            "skip": "offset",
-            "select": "select",
-            "count": "count",
+        return result
+
+    @staticmethod
+    def convert_order_by_value(value: str) -> str:
+        return value.replace(" ", ".")
+
+    @staticmethod
+    def convert_expand(expand_query: str, previous_entity: str = None) -> str:
+        result = STA2REST.split_but_not_between(expand_query, ",", "(", ")")
+    
+        entities = []
+        additionals = []
+        for entity in result:
+            converted_entity = ""
+            if "(" in entity:
+                entity, subquery = entity.split("(",1)
+                c_entity = STA2REST.convert_entity(entity)
+                converted_entity = c_entity + "("
+                subquery = subquery[:-1]
+                actions = STA2REST.split_but_not_between(subquery, ";", "(", ")")
+                for action in actions:
+                    param, value = action.split("=",1)
+                    converted_param = STA2REST.convert_query_param(param)
+                    if param == "$expand":
+                        result = STA2REST.convert_expand(value, c_entity)
+                        entities.extend(result["select"].split(","))
+                        additionals += result["additionals"]
+                    elif param == "$select":
+                        converted_entity += value
+                    else:
+                        # Adjust values
+                        if param == "$orderby":
+                            value = STA2REST.convert_order_by_value(value)
+                        additionals.append(f"{previous_entity}.{c_entity}.{converted_param}={value}")
+            else:
+                converted_entity = STA2REST.convert_entity(entity) + "(*"
+            
+            converted_entity += ")"
+            
+            entities.append(converted_entity)
+
+        return {
+            "select": ",".join(entities),
+            "additionals": additionals
         }
-        return query_param_mapping.get(query_param, query_param)
 
     @staticmethod
     def convert_query(sta_query: str) -> str:
-        """
-        Convert a SensorThings query to its corresponding representation.
-
-        Args:
-            sta_query (str): The SensorThings query to convert.
-
-        Returns:
-            str: The corresponding converted query.
-        """
-        sta_query = sta_query.replace("$", "").split("&")
-        converted_query_params = []
+        sta_query = sta_query.split("&")
+        converted_query_params = {}
         for query_param in sta_query:
-            key, value = query_param.split("=")
+            key, value = query_param.split("=", 1)
+            if key == "$expand":
+                converted_value = STA2REST.convert_expand(value)
+            else:
+                converted_value = value
             converted_key = STA2REST.convert_query_param(key)
-            # TODO: Check for strange query parameters
-            converted_query_params.append(f"{converted_key}={value}")
-        return "&".join(converted_query_params)
+            converted_query_params[converted_key] = converted_value
+
+        # Check for expand and if present merge the select
+        if "expand" in converted_query_params and "select" in converted_query_params["expand"]:
+            converted_query_params["select"] += "," + converted_query_params["expand"]["select"]
+        
+        # Get additionals and remove them from the query
+        additionals = []
+        if "expand" in converted_query_params and "additionals" in converted_query_params["expand"]:
+            additionals = converted_query_params["expand"]["additionals"]
+            del converted_query_params["expand"]
+
+        # merge in format key=value&key=value 
+        converted_query = "&".join([f"{key}={value}" for key, value in converted_query_params.items()])
+        
+        for additional in additionals:
+            converted_query += "&" + additional
+        
+        return converted_query
