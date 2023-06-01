e9ad3fef9350e07ab3e2d01aaaa29eab6ea6bbf0
filippofinke <filippofinke@protonmail.ch>
2023-06-01 10:51:21 +0200

    feat: add filter unit tests

--- src/sta2rest/test.py
+++ src/sta2rest/test.py
@@ -39,7 +39,6 @@ class STA2RESTTestCase(unittest.TestCase):
 
     def test_convert_query_param(self):
         query_param_mappings = {
-            "$filter": "filter",
             "$orderby": "order",
             "$top": "limit",
             "$skip": "offset"
@@ -52,17 +51,17 @@ class STA2RESTTestCase(unittest.TestCase):
     def test_convert_sensor_things_query(self):
         query_mappings = {
             "$filter=type eq 'temperature'&$orderby=timestamp desc&$top=10&$skip=5":
-                "filter=type eq 'temperature'&order=timestamp.desc&limit=10&offset=5",
+                "type=eq.'temperature'&order=timestamp.desc&limit=10&offset=5",
             "$filter=type eq 'humidity'&$top=5":
-                "filter=type eq 'humidity'&limit=5",
+                "type=eq.'humidity'&limit=5",
             "$orderby=timestamp asc&$skip=2":
                 "order=timestamp.asc&offset=2",
             "$select=id,name,description,properties&$top=1000&$filter=properties/type eq 'station'&$expand=Locations,Datastreams($select=id,name,unitOfMeasurement;$expand=ObservedProperty($select=name),Observations($select=result,phenomenonTime;$orderby=phenomenonTime desc;$top=1))":
-            "select=id,name,description,properties,Location(*),ObservedProperty(name),Observation(result,phenomenonTime),Datastream(id,name,unitOfMeasurement)&limit=1000&filter=properties/type eq 'station'&Datastream.Observation.order=phenomenonTime.desc&Datastream.Observation.limit=1",
+            "select=id,name,description,properties,Location(*),ObservedProperty(name),Observation(result,phenomenonTime),Datastream(id,name,unitOfMeasurement)&limit=1000&properties->>type=eq.'station'&Datastream.Observation.order=phenomenonTime.desc&Datastream.Observation.limit=1",
             "$select=@iot.id,description&$expand=Datastreams($select=@iot.id,description)": "select=@iot.id,description,Datastream(@iot.id,description)",
             "$expand=Datastreams": "select=Datastream(*)",
             "$expand=Observations,ObservedProperty": "select=Observation(*),ObservedProperty(*)",
-            "$expand=Observations($filter=result eq 1)": "select=Observation(*)&Observation.filter=result eq 1",
+            "$expand=Observations($filter=result eq 1)": "select=Observation(*)&Observation.result=eq.1",
             "$expand=Observations($select=result)": "select=Observation(result)",
             "$select=result,resultTime": "select=result,resultTime",
             "$orderby=result": "order=result",
@@ -71,7 +70,7 @@ class STA2RESTTestCase(unittest.TestCase):
             "$top=5&$orderby=phenomenonTime%20desc": "limit=5&order=phenomenonTime.desc",
             "$skip=5": "offset=5",
             "$count=true": "count=true",
-            "$filter=result lt 10.00": "filter=result lt 10.00",
+            "$filter=result lt 10.00": "result=lt.10.00",
         }
 
         for query, expected in query_mappings.items():
