7043f021bb21d3e14989f54f63fd3626954adc20
filippofinke <filippofinke@protonmail.ch>
2023-06-28 11:28:00 +0200

    refactor: remove unused operators (for now)

--- src/sta2rest/filter_visitor.py
+++ src/sta2rest/filter_visitor.py
@@ -1,11 +1,16 @@
+"""
+Module: STA2REST filter visitor
+
+Author: Filippo Finke
+
+This module provides a visitor for the filter AST.
+"""
 from odata_query import ast, visitor
 
 class FilterVisitor(visitor.NodeVisitor):
-     
-     def visit_Add(self, node: ast.Add) -> str:
-        print("Add")
-        print(node)
-        return node
+     """
+       Visitor for the filter AST.  
+     """
      
      def visit_All(self, node: ast.All) -> str:
         return "all"
@@ -17,13 +22,9 @@ class FilterVisitor(visitor.NodeVisitor):
         return "any"
      
      def visit_Attribute(self, node: ast.Attribute) -> str:
-        print("Attribute")
-        print(node)
         return node
      
      def visit_BinOp(self, node: ast.BinOp) -> str:
-        print("BinOp")
-        print(node)
         return node
      
      def visit_BoolOp(self, node: ast.BoolOp) -> str:
@@ -37,21 +38,6 @@ class FilterVisitor(visitor.NodeVisitor):
          left = left.replace("=", ".")
          right = right.replace("=", ".")
          return f"{operator}=({left},{right})"
-     
-     def visit_Boolean(self, node: ast.Boolean) -> str:
-        print("Boolean")
-        print(node)
-        return node
-     
-     def visit_Call(self, node: ast.Call) -> str:
-        print("Call")
-        print(node)
-        return node
-     
-     def visit_CollectionLambda(self, node: ast.CollectionLambda) -> str:
-        print("CollectionLambda")
-        print(node)
-        return node
      
      def visit_Compare(self, node: ast.Compare) -> str:
         
@@ -73,34 +59,15 @@ class FilterVisitor(visitor.NodeVisitor):
 
         return f"{left}={comparator}.{right}"
      
-     def visit_Date(self, node: ast.Date) -> str:
-        print("Date")
-        print(node)
-        return node
-     
+
      def visit_DateTime(self, node: ast.DateTime) -> str:
         return node.val
-     
-     def visit_Div(self, node: ast.Div) -> str:
-        print("Div")
-        print(node)
-        return node
-     
-     def visit_Duration(self, node: ast.Duration) -> str:
-        print("Duration")
-        print(node)
-        return node
-     
+          
      def visit_Eq(self, node: ast.Eq) -> str:
         return "eq"
      
      def visit_Float(self, node: ast.Float) -> str:
         return node.val
-     
-     def visit_GUID(self, node: ast.GUID) -> str:
-        print("GUID")
-        print(node)
-        return node
      
      def visit_Gt(self, node: ast.Gt) -> str:
         return "gt"
@@ -114,16 +81,6 @@ class FilterVisitor(visitor.NodeVisitor):
      def visit_Integer(self, node: ast.Integer) -> str:
         return node.val
      
-     def visit_Lambda(self, node: ast.Lambda) -> str:
-        print("Lambda")
-        print(node)
-        return node
-     
-     def visit_List(self, node: ast.List) -> str:
-        print("List")
-        print(node)
-        return node
-     
      def visit_Lt(self, node: ast.Lt) -> str:
         return "lt"
      
@@ -136,36 +93,11 @@ class FilterVisitor(visitor.NodeVisitor):
      def visit_NotEq(self, node: ast.NotEq) -> str:
         return "neq"
      
-     def visit_Null(self, node: ast.Null) -> str:
-        print("Null")
-        print(node)
-        return node
-     
      def visit_Or(self, node: ast.Or) -> str:
         return "or"
      
      def visit_String(self, node: ast.String) -> str:
         return node.val
-     
-     def visit_Sub(self, node: ast.Sub) -> str:
-        print("Sub")
-        print(node)
-        return node
-     
-     def visit_Time(self, node: ast.Time) -> str:
-        print("Time")
-        print(node)
-        return node
-     
-     def visit_USub(self, node: ast.USub) -> str:
-        print("USub")
-        print(node)
-        return node
-     
-     def visit_UnaryOp(self, node: ast.UnaryOp) -> str:
-        print("UnaryOp")
-        print(node)
-        return node
 
      def visit_Identifier(self, node: ast.Identifier) -> str:
         return node
