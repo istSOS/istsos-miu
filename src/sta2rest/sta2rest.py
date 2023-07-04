cfc16e598bd36e9c194be0601107606d0a4f7808
filippofinke <filippofinke@protonmail.ch>
2023-07-04 15:09:45 +0200

    feat: use new converter library

--- src/sta2rest/sta2rest.py
+++ src/sta2rest/sta2rest.py
@@ -6,20 +6,157 @@ Author: Filippo Finke
 This module provides utility functions to convert various elements used in SensorThings queries to their corresponding
 representations in a REST API.
 """
-import re 
-import urllib.parse
 from odata_query.grammar import ODataLexer
 from odata_query.grammar import ODataParser
 from filter_visitor import FilterVisitor
+from sta_parser.lexer import Lexer
+from sta_parser.visitor import Visitor
+from sta_parser.parser import Parser
+from sta_parser import ast
 
 odata_filter_lexer = ODataLexer()
 odata_filter_parser = ODataParser()
 
+class NodeVisitor(Visitor):
+    def visit_IdentifierNode(self, node: ast.IdentifierNode):
+        return node.name
+
+    def visit_SelectNode(self, node: ast.SelectNode):
+        identifiers = ','.join([self.visit(identifier) for identifier in node.identifiers])
+        return f'select={identifiers}'
+
+    def visit_FilterNode(self, node: ast.FilterNode):
+        ast = odata_filter_parser.parse(odata_filter_lexer.tokenize(node.filter))
+        # Visit the tree to convert the filter
+        res = FilterVisitor().visit(ast)
+        return res
+
+    def visit_OrderByNodeIdentifier(self, node: ast.OrderByNodeIdentifier):
+        return f'{node.identifier}.{node.order}'
+
+    def visit_OrderByNode(self, node: ast.OrderByNode):
+        identifiers = ','.join([self.visit(identifier) for identifier in node.identifiers])
+        return f'order={identifiers}'
+
+    def visit_SkipNode(self, node: ast.SkipNode):
+        return f'offset={node.count}'
+
+    def visit_TopNode(self, node: ast.TopNode):
+        return f'limit={node.count}'
+
+    def visit_CountNode(self, node: ast.CountNode):
+        return f'count={node.value}'
+    
+    def visit_ExpandNode(self, node: ast.ExpandNode, parent=None):
+
+        select = None
+        filter = ""
+        orderby = ""
+        skip = ""
+        top = ""
+        count = ""
+
+        for expand_identifier in node.identifiers:
+                expand_identifier.identifier = STA2REST.convert_entity(expand_identifier.identifier)
+                prefix = ""
+                if parent:
+                    prefix = parent
+                prefix += expand_identifier.identifier + "."
+        
+                if expand_identifier.subquery:
+                    if expand_identifier.subquery.select:
+                        if not select:
+                            select = ast.SelectNode([])
+                        identifiers = ','.join([self.visit(identifier) for identifier in expand_identifier.subquery.select.identifiers])
+                        select.identifiers.append(ast.IdentifierNode(f'{expand_identifier.identifier}({identifiers})'))
+                    if expand_identifier.subquery.filter:
+                        result = self.visit_FilterNode(expand_identifier.subquery.filter)
+                        filter = prefix + result
+                    if expand_identifier.subquery.orderby:
+                        orderby = prefix + "order=" + ','.join([self.visit(identifier) for identifier in expand_identifier.subquery.orderby.identifiers])
+                    if expand_identifier.subquery.skip:
+                        skip = prefix + "offset=" + str(expand_identifier.subquery.skip.count)
+                    if expand_identifier.subquery.top:
+                        top = prefix + "limit=" + str(expand_identifier.subquery.top.count)
+                    if expand_identifier.subquery.count:
+                        count = prefix + "count=" + str(expand_identifier.subquery.count.value).lower()
+
+                    if expand_identifier.subquery.expand:
+                        result = self.visit_ExpandNode(expand_identifier.subquery.expand, prefix)
+                        if result['select']:
+                            if not select:
+                                select = ast.SelectNode([])
+                            select.identifiers.extend(result['select'].identifiers)
+                        if result['orderby']:
+                            if orderby:
+                                orderby += "&"
+                            orderby += result['orderby']
+                        if result['skip']:
+                            if skip:
+                                skip += "&"
+                            skip += result['skip']
+                        if result['top']:
+                            if top:
+                                top += "&"
+                            top += result['top']
+                        if result['count']:
+                            if count:
+                                count += "&"
+                            count += result['count']
+                        if result['filter']:
+                            if filter:
+                                filter += "&"
+                            filter += result['filter']
+                
+                if not expand_identifier.subquery or not expand_identifier.subquery.select:
+                    if not select:
+                        select = ast.SelectNode([])
+                    select.identifiers.append(ast.IdentifierNode(f'{expand_identifier.identifier}(*)'))
+        return {
+            'select': select,
+            'filter': filter,
+            'orderby': orderby,
+            'skip': skip,
+            'top': top,
+            'count': count
+        }
+
+    def visit_QueryNode(self, node: ast.QueryNode):
+        query_parts = []
+
+        if node.expand:
+            result = self.visit(node.expand)
+            if result['select']:
+                if not node.select:
+                    node.select = ast.SelectNode([])
+                node.select.identifiers.extend(result['select'].identifiers)
+            if result['orderby']:
+                query_parts.append(result['orderby'])
+            if result['skip']:
+                query_parts.append(result['skip'])
+            if result['top']:
+                query_parts.append(result['top'])
+            if result['count']:
+                query_parts.append(result['count'])
+            if result['filter']:
+                query_parts.append(result['filter'])
+
+        if node.select:
+            query_parts.append(self.visit(node.select))
+        if node.filter:
+            query_parts.append(self.visit(node.filter))
+        if node.orderby:
+            query_parts.append(self.visit(node.orderby))
+        if node.skip:
+            query_parts.append(self.visit(node.skip))
+        if node.top:
+            query_parts.append(self.visit(node.top))
+        if node.count:
+            query_parts.append(self.visit(node.count).lower())
+        
+        return '&'.join(query_parts)
+
 class STA2REST:
-    """
-    This class provides utility functions to convert various elements used in SensorThings queries to their corresponding
-    representations in a compatible PostgREST REST API.
-    """
 
     # Mapping from SensorThings entities to their corresponding database table names
     ENTITY_MAPPING = {
@@ -32,27 +169,6 @@ class STA2REST:
         "FeaturesOfInterest": "FeatureOfInterest",
         "HistoricalLocations": "HistoricalLocation",
     }
-
-    # Mapping from SensorThings properties to their corresponding database column names
-    PROPERTY_MAPPING = {
-        "name": "name",
-        "description": "description",
-        "encodingType": "encodingType",
-        "metadata": "metadata",
-        "definition": "definition",
-    }
-
-    # Mapping from SensorThings query parameters to their corresponding PostgRESTR query parameters
-    QUERY_PARAM_MAPPING = {
-        "$filter": "filter",
-        "$orderby": "order",
-        "$top": "limit",
-        "$skip": "offset",
-        "$select": "select",
-        "$count": "count",
-        "$expand": "expand",
-    }
-
     @staticmethod
     def convert_entity(entity: str) -> str:
         """
@@ -65,249 +181,16 @@ class STA2REST:
             str: The converted entity name in REST format.
         """
         return STA2REST.ENTITY_MAPPING.get(entity, entity)
-
-    @staticmethod
-    def convert_property(property_name: str) -> str:
-        """
-        Converts a property name from STA format to REST format.
-
-        Args:
-            property_name (str): The property name in STA format.
-
-        Returns:
-            str: The converted property name in REST format.
-        """
-        return STA2REST.PROPERTY_MAPPING.get(property_name, property_name)
-
-    @staticmethod
-    def convert_query_param(query_param: str) -> str:
-        """
-        Converts a query parameter name from STA format to REST format.
-
-        Args:
-            query_param (str): The query parameter name in STA format.
-
-        Returns:
-            str: The converted query parameter name in REST format.
-        """
-        return STA2REST.QUERY_PARAM_MAPPING.get(query_param, query_param)
-
-    @staticmethod
-    def split_but_not_between(query: str, split: str, delim1: str, delim2: str) -> list:
-        """
-        Splits a string by a given character, but only if the character is not between two other given characters.
-
-        Args:
-            query (str): The string to split.
-            split (str): The character to split by.
-            delim1 (str): The first delimiter character.
-            delim2 (str): The second delimiter character.
-
-        Returns:
-            list: The list of substrings.
-        """
-
-        result = []
-        stack = []
-        current = ''
-        for char in query:
-            # Check if the current character is a delimiter
-            if char == delim1:
-                current += char
-                stack.append(delim1)
-            # Check if the current character is a delimiter
-            elif char == delim2:
-                current += char
-                # Check if the current delimiter is the same as the last one in the stack
-                if stack and stack[-1] == delim1:
-                    stack.pop()
-            elif char == split:
-                if stack:
-                    # Inside parentheses, treat comma as part of the current substring
-                    current += char
-                else:
-                    # Outside parentheses, split the string and reset the current substring
-                    result.append(current.strip())
-                    current = ''
-            else:
-                # Regular character, add it to the current substring
-                current += char
-        
-        # Add the last substring
-        if current:
-            result.append(current.strip())
-
-        return result
-
-    @staticmethod
-    def convert_order_by_value(value: str) -> str:
-        """
-        Converts an order by value from STA format to REST format.
-
-        Args:
-            value (str): The order by value in STA format.
-
-        Returns:
-            str: The converted order by value in REST format.
-        """
-
-        # strip all the spaces after commas ",   " -> ","
-        value = re.sub(r",\s+", ",", value)
-        # replace space and slash with dot
-        value = value.replace(" ", ".").replace("/", ".")
-        return value
-
-    @staticmethod
-    def convert_filter_by_value(value: str) -> str:
-        """
-        Converts a filter by value from STA format to REST format.
-
-        see https://docs.ogc.org/is/18-088/18-088.html#_built_in_filter_operations
-        see https://postgrest.org/en/stable/references/api/tables_views.html#logical-operators
-
-        Args:
-            value (str): The filter by value in STA format.
-
-        Returns:
-            str: The converted filter by value in REST format.
-        """
-        # Get the AST tree from the filter
-        ast = odata_filter_parser.parse(odata_filter_lexer.tokenize(value))
-        # Visit the tree to convert the filter
-        res = FilterVisitor().visit(ast)
-        return res
-
-    @staticmethod
-    def convert_expand(expand_query: str, previous_entity: str = None) -> str:
-        """
-        Converts an expand query from STA format to REST format.
-
-        Args:
-            expand_query (str): The expand query in STA format.
-            previous_entity (str, optional): The previous entity name. Defaults to None.
-
-        Returns:
-            str: The converted expand query in REST format.
-        """
-
-        # split by comma but not between parentheses
-        result = STA2REST.split_but_not_between(expand_query, ",", "(", ")")
-    
-        entities = []
-        additionals = []
-        for entity in result:
-            converted_entity = ""
-            has_select = False
-            # Check if the entity has a subquery
-            if "(" in entity:
-                entity, subquery = entity.split("(",1)
-                c_entity = STA2REST.convert_entity(entity)
-                converted_entity = c_entity + "("
-                subquery = subquery[:-1]
-                # Get all the actions in the subquery
-                actions = STA2REST.split_but_not_between(subquery, ";", "(", ")")
-                for action in actions:
-                    param, value = action.split("=",1)
-                    converted_param = STA2REST.convert_query_param(param)
-                    if param == "$expand":
-                        # Recursively convert the subquery
-                        result = STA2REST.convert_expand(value, c_entity)
-                        entities.extend(result["select"].split(","))
-                        additionals += result["additionals"]
-                    elif param == "$select":
-                        has_select = True
-                        converted_entity += value
-                    else:
-                        # Adjust values
-                        if param == "$orderby":
-                            value = STA2REST.convert_order_by_value(value)
-                        elif param == "$filter":
-                            value = STA2REST.convert_filter_by_value(value)
-
-                        # Add the previous entity if present
-                        p_entity = previous_entity + "." if previous_entity != None else ""
-
-                        # Add the action to the additionals
-                        if param != "$filter":
-                            additionals.append(f"{p_entity}{c_entity}.{converted_param}={value}")
-                        else:
-                            additionals.append(f"{p_entity}{c_entity}.{value}")
-            
-            if not has_select:
-                converted_entity = STA2REST.convert_entity(entity) + "(*"
-            
-            converted_entity += ")"
-            
-            entities.append(converted_entity)
-
-        return {
-            "select": ",".join(entities),
-            "additionals": additionals
-        }
-
+  
     @staticmethod
     def convert_query(sta_query: str) -> str:
-        """
-        Converts a query from STA format to REST format.
+        lexer = Lexer(sta_query)
+        tokens = lexer.tokenize()
+        parser = Parser(tokens)
+        ast = parser.parse()
+        visitor = NodeVisitor()
+        return visitor.visit(ast)
 
-        Args:
-            sta_query (str): The query in STA format.
-
-        Returns:
-            str: The converted query in REST format.
-        """
-
-        # remove unwanted characters from the query
-        sta_query = urllib.parse.unquote(sta_query)
-
-        # get the query parameters
-        sta_query = sta_query.split("&")
-        converted_query_params = {}
-
-        # convert the query parameters
-        for query_param in sta_query:
-            key, value = query_param.split("=", 1)
-            if key == "$expand":
-                converted_value = STA2REST.convert_expand(value)
-            else:
-                if key == "$orderby":
-                    value = STA2REST.convert_order_by_value(value)
-                elif key == "$filter":
-                    value = STA2REST.convert_filter_by_value(value)
-                converted_value = value
-            converted_key = STA2REST.convert_query_param(key)
-            converted_query_params[converted_key] = converted_value
-
-        # Check for expand and if present merge the select
-        if "expand" in converted_query_params and "select" in converted_query_params["expand"]:
-            if "select" in converted_query_params:
-                converted_query_params["select"] += "," + converted_query_params["expand"]["select"]
-            else:
-                converted_query_params["select"] = converted_query_params["expand"]["select"]
-        
-        # Get additionals parameters and remove them from the query
-        additionals = []
-        if "expand" in converted_query_params and "additionals" in converted_query_params["expand"]:
-            additionals = converted_query_params["expand"]["additionals"]
-            del converted_query_params["expand"]
-
-        # Check if the filter is present and move it to the additionals
-        if "filter" in converted_query_params:
-            additionals.append(converted_query_params["filter"])
-            del converted_query_params["filter"]
-
-        # Merge in format key=value&key=value 
-        converted_query = "&".join([f"{key}={value}" for key, value in converted_query_params.items()])
-        
-        # Add additionals
-        for additional in additionals:
-            converted_query += "&" + additional
-
-        # Remove the first & if present
-        if converted_query.startswith("&"):
-            converted_query = converted_query[1:]
-        
-        return converted_query
 
 if __name__ == "__main__":
     """
@@ -315,6 +198,6 @@ if __name__ == "__main__":
 
     This example converts a STA query to a REST query.
     """
-    query = "$filter=location_id eq 2 and id eq 2"
+    query = "$expand=Observations($filter=result eq 1)"
     print("QUERY", query)
     print("CONVERTED", STA2REST.convert_query(query))
--- src/sta2rest/test.py
+++ src/sta2rest/test.py
@@ -31,60 +31,29 @@ class STA2RESTTestCase(unittest.TestCase):
         for entity, expected in entity_mappings.items():
             self.assertEqual(STA2REST.convert_entity(entity), expected)
 
-    def test_convert_property(self):
-        """
-        Test the conversion of properties.
-        """
-
-        property_mappings = {
-            "name": 'name',
-            "description": 'description',
-            "encodingType": 'encodingType',
-            "metadata": 'metadata'
-            # Add more property mappings as needed
-        }
-
-        for prop, expected in property_mappings.items():
-            self.assertEqual(STA2REST.convert_property(prop), expected)
-
-    def test_convert_query_param(self):
-        """
-        Test the conversion of query parameters.
-        """
-
-        query_param_mappings = {
-            "$orderby": "order",
-            "$top": "limit",
-            "$skip": "offset"
-            # Add more query parameter mappings as needed
-        }
-
-        for param, expected in query_param_mappings.items():
-            self.assertEqual(STA2REST.convert_query_param(param), expected)
-
     def test_convert_sensor_things_query(self):
         """
         Test the conversion of sensor things queries.
         """
         query_mappings = {
             "$filter=type eq 'temperature'&$orderby=timestamp desc&$top=10&$skip=5":
-                "order=timestamp.desc&limit=10&offset=5&type=eq.temperature",
+                "type=eq.temperature&order=timestamp.desc&offset=5&limit=10",
             "$filter=type eq 'humidity'&$top=5":
-                "limit=5&type=eq.humidity",
+                "type=eq.humidity&limit=5",
             "$orderby=timestamp asc&$skip=2":
                 "order=timestamp.asc&offset=2",
             "$select=id,name,description,properties&$top=1000&$filter=properties/type eq 'station'&$expand=Locations,Datastreams($select=id,name,unitOfMeasurement;$expand=ObservedProperty($select=name),Observations($select=result,phenomenonTime;$orderby=phenomenonTime desc;$top=1))":
-            "select=id,name,description,properties,Location(*),ObservedProperty(name),Observation(result,phenomenonTime),Datastream(id,name,unitOfMeasurement)&limit=1000&Datastream.Observation.order=phenomenonTime.desc&Datastream.Observation.limit=1&properties->>type=eq.station",
-            "$select=@iot.id,description&$expand=Datastreams($select=@iot.id,description)": "select=@iot.id,description,Datastream(@iot.id,description)",
+            "Datastream.Observation.order=phenomenonTime.desc&Datastream.Observation.limit=1&select=id,name,description,properties,Location(*),Datastream(id,name,unitOfMeasurement),ObservedProperty(name),Observation(result,phenomenonTime)&properties->>type=eq.station&limit=1000",
+            "$select=id,description&$expand=Datastreams($select=id,description)": "select=id,description,Datastream(id,description)",
             "$expand=Datastreams": "select=Datastream(*)",
             "$expand=Observations,ObservedProperty": "select=Observation(*),ObservedProperty(*)",
-            "$expand=Observations($filter=result eq 1)": "select=Observation(*)&Observation.result=eq.1",
+            "$expand=Observations($filter=result eq 1)": "Observation.result=eq.1&select=Observation(*)",
             "$expand=Observations($select=result)": "select=Observation(result)",
-            "$select=result,resultTime": "select=result,resultTime",
-            "$orderby=result": "order=result",
-            "$expand=Datastream&$orderby=Datastreams/id desc,phenomenonTime": "order=Datastreams.id.desc,phenomenonTime&select=Datastream(*)",
+            "$select=result": "select=result",
+            "$orderby=result": "order=result.asc",
+            "$expand=Datastream&$orderby=Datastreams/id desc,phenomenonTime": "select=Datastream(*)&order=Datastreams/id.desc,phenomenonTime.asc",
             "$top=5": "limit=5",
-            "$top=5&$orderby=phenomenonTime%20desc": "limit=5&order=phenomenonTime.desc",
+            "$top=5&$orderby=phenomenonTime%20desc": "order=phenomenonTime.desc&limit=5",
             "$skip=5": "offset=5",
             "$count=true": "count=true",
             "$filter=result lt 10.00": "result=lt.10.00",
@@ -103,6 +72,7 @@ class STA2RESTTestCase(unittest.TestCase):
         }
 
         for query, expected in query_mappings.items():
+            print("QUERY", query)
             self.assertEqual(STA2REST.convert_query(query), expected)
 
 if __name__ == '__main__':
