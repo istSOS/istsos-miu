3c13fd38cc278055abef7021b6508c5fcad3acaf
filippofinke <filippofinke@protonmail.ch>
2023-07-04 19:51:59 +0200

    feat: add try and catch

--- fastapi/app/v1/fetch_postgrest.py
+++ fastapi/app/v1/fetch_postgrest.py
@@ -17,16 +17,19 @@ app = FastAPI()
 # print(p)
 @app.get("/Datastream")
 async def root(request: Request):
+    try:
+        # Get the original query parameters from the request
+        query_params = urllib.parse.unquote_plus(str(request.query_params))
+        print("Original query: ", query_params)
+        # Convert the STA query to a PostgREST query
+        converted_query = STA2REST.convert_query(query_params)
+        print("Converted query: ", converted_query)
 
-    # Get the original query parameters from the request
-    query_params = urllib.parse.unquote_plus(str(request.query_params))
-    print("Original query: ", query_params)
-    # Convert the STA query to a PostgREST query
-    converted_query = STA2REST.convert_query(query_params)
-    print("Converted query: ", converted_query)
+        # Send the converted query to PostgREST
+        r = requests.get('http://localhost:3000/Datastream?' + converted_query)
 
-    # Send the converted query to PostgREST
-    r = requests.get('http://localhost:3000/Datastream?' + converted_query)
-
-    # Return the response from PostgREST
-    return r.json()
+        # Return the response from PostgREST
+        return r.json()
+    except Exception as e:
+        # send the error message to the client
+        return {"error": str(e)}
