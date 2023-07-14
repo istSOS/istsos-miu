f86336e88ae905b082e795b70c284a282d319f4c
filippofinke <filippofinke@protonmail.ch>
2023-07-14 15:07:22 +0200

    fix: format response

--- fastapi/app/v1/endpoints/general.py
+++ fastapi/app/v1/endpoints/general.py
@@ -18,6 +18,15 @@ def __flatten_navigation_links(row):
         row.update(row["@iot.navigationLink"])
         del row["@iot.navigationLink"]
 
+def __flatten_expand_entity(data):
+    # Check if there is only one key and it is in an ENTITY_MAPPING from the sta2rest module
+    if len(data[0].keys()) == 1 and list(data[0].keys())[0] in sta2rest.STA2REST.ENTITY_MAPPING:
+        # Get the value of the first key
+        key_name = list(data[0].keys())[0]
+        data = data[0][key_name]
+
+    return data
+
 @v1.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(request: Request, path_name: str):
     try:
@@ -59,7 +68,7 @@ async def catch_all(request: Request, path_name: str):
             data = r.json()
 
             if result['single_result']:
-                data = data[0]
+                data = __flatten_expand_entity(data)[0]
                 if result['value']:
                     # get the value of the first key
                     data = data[list(data.keys())[0]]  
@@ -78,7 +87,8 @@ async def catch_all(request: Request, path_name: str):
                         "@iot.selfLink": row["@iot.selfLink"]
                     })
             else:
-                # Flatten the @iot.navigationLink for each row
+                data = __flatten_expand_entity(data)
+
                 for row in data:
                     __flatten_navigation_links(row)
 
