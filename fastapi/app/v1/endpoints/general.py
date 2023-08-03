93a5860570ccdfc52dd2ea0091076878d21b904b
filippofinke <filippofinke@protonmail.ch>
2023-08-03 16:58:34 +0200

    feat: get ids

--- fastapi/app/v1/endpoints/general.py
+++ fastapi/app/v1/endpoints/general.py
@@ -268,6 +268,7 @@ async def create_entity(entity_name, body):
 
     body = flatten_entity_body(body, body)
 
+
     # Creation order
     created_ids = {}
     creation_order = ["Thing", "Location", "Sensor", "ObservedProperty", "FeaturesOfInterest", "Datastream", "Observation"]
@@ -275,10 +276,24 @@ async def create_entity(entity_name, body):
         if entity_name in body:
             formatted_body = format_entity_body(body[entity_name])
 
+            # check if the entity has sub entities and if they are empty check if id is present
+            if isinstance(formatted_body, list):
+                for item in formatted_body:
+                    for key in item:
+                        if key in created_ids:
+                            item[key] = created_ids[key]
+            else:
+                for key in formatted_body:
+                    # check if key is present in created_ids
+                    if key in created_ids:
+                        formatted_body[key] = created_ids[key]
+
             url = "http://postgrest:3000/" + entity_name
 
-            print("CREATING ENTITY", entity_name, formatted_body)
-            '''
+            print("Creating entity: ", entity_name)
+            print("Body: ", formatted_body)
+
+            """
             async with httpx.AsyncClient() as client:   
                 # post to postgrest
                 r = await client.post(url, json=formatted_body, headers={"Prefer": "return=representation"})
@@ -290,7 +305,14 @@ async def create_entity(entity_name, body):
                 if r.status_code != 201:
                     raise PostgRESTError(result["message"])
                 
-                return result'''
+                # get first element of the result
+                result = result[0]
+
+                # get the id of the created entity
+                id_key = sta2rest.STA2REST.convert_to_database_id(entity_name)
+                created_ids[id_key] = result["id"]
+
+                print("Created entity: ", id_key, " with id: ", result["id"])"""
 
     return None
 
