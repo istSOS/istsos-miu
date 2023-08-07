c4279650c15617025f1be60c8060f249cd7f5fc8
Filippo Finke <filippofinke@protonmail.ch>
2023-08-07 21:16:35 +0200

    feat: add settings

---
+++ fastapi/app/settings.py
@@ -0,0 +1,6 @@
+tables = ["Datastreams", "FeaturesOfInterest", "HistoricalLocations", "Locations", "Observations", "ObservedProperties", "Sensors", "Things"]
+serverSettings = {
+    "conformance": [
+        "http://www.opengis.net/spec/iot_sensing/1.1/req/request-data",
+    ],
+}
