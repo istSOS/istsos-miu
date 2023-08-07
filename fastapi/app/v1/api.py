c84b8a8917d66ca3c049b0bd0695e793ddc145c8
Filippo Finke <filippofinke@protonmail.ch>
2023-08-07 23:23:05 +0200

    feat: add update endpoint

--- fastapi/app/v1/api.py
+++ fastapi/app/v1/api.py
@@ -1,6 +1,7 @@
 from app.v1.endpoints import read
 from app.v1.endpoints import insert
 from app.v1.endpoints import delete
+from app.v1.endpoints import update_patch
 from fastapi import FastAPI
 
 v1 = FastAPI()
@@ -9,3 +10,4 @@ v1 = FastAPI()
 v1.include_router(read.v1)
 v1.include_router(insert.v1)
 v1.include_router(delete.v1)
+v1.include_router(update_patch.v1)
