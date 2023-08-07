62112b69963ca24dbe7f1cae7a4b0dc0cb401e2c
Filippo Finke <filippofinke@protonmail.ch>
2023-08-07 21:40:42 +0200

    feat: add delete route

--- fastapi/app/v1/api.py
+++ fastapi/app/v1/api.py
@@ -1,5 +1,6 @@
 from app.v1.endpoints import read
 from app.v1.endpoints import insert
+from app.v1.endpoints import delete
 from fastapi import FastAPI
 
 v1 = FastAPI()
@@ -7,3 +8,4 @@ v1 = FastAPI()
 # Register the endpoints
 v1.include_router(read.v1)
 v1.include_router(insert.v1)
+v1.include_router(delete.v1)
---
+++ fastapi/app/v1/endpoints/delete.py
@@ -0,0 +1,60 @@
+import traceback
+import httpx
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from fastapi import status
+from app.sta2rest import sta2rest
+from app.utils.utils import PostgRESTError
+
+v1 = APIRouter()
+
+# Handle DELETE requests
+@v1.api_route("/{path_name:path}", methods=["DELETE"])
+async def catch_all_delete(request: Request, path_name: str):
+
+    try:
+        full_path = request.url.path
+        # parse uri
+        result = sta2rest.STA2REST.parse_uri(full_path)
+
+        # Get main entity
+        [name, id] = result["entity"]
+        
+        # Get the name and id
+        if not name:
+            raise Exception("No entity name provided")
+    
+        if not id:
+            raise Exception("No entity id provided")
+
+        async with httpx.AsyncClient() as client:   
+            url = "http://postgrest:3000/" + name + "?id=eq." + id
+
+            # post to postgrest
+            r = await client.delete(url)
+
+
+            if r.status_code != 204:
+                result = r.json()
+                raise PostgRESTError(result["message"])
+
+        # Return okay
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "code": 200,
+                "type": "success"
+            }
+        )
+        
+    except Exception as e:
+        # print stack trace
+        traceback.print_exc()
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "code": 400,
+                "type": "error",
+                "message": str(e)
+            }
+        )
