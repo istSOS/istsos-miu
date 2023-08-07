f37454779add1ca20e9a8a35b0509da740f5b565
Filippo Finke <filippofinke@protonmail.ch>
2023-08-07 21:23:00 +0200

    feat: add comments

--- fastapi/app/utils/utils.py
+++ fastapi/app/utils/utils.py
@@ -2,9 +2,22 @@ import httpx
 from app.sta2rest import sta2rest
 
 class PostgRESTError(Exception):
+    """
+    Exception raised for errors in the PostgREST response.
+    """
     pass
 
 def get_result_type_and_column(input_string):
+    """
+    Get the result type and the column name for the result
+
+    Args:
+        input_string (str): The input string
+
+    Returns:
+        int: The result type
+        str: The column name
+    """
     try:
         value = eval(str(input_string))
     except (SyntaxError, NameError):
@@ -34,6 +47,17 @@ def get_result_type_and_column(input_string):
         raise Exception("Cannot cast result to a valid type")
     
 def flatten_entity_body(entity, body, name = None):
+    """
+    Flatten the entity body
+
+    Args:
+        entity (dict): The entity
+        body (dict): The body
+        name (str, optional): The name of the entity. Defaults to None.
+
+    Returns:
+        dict: The flattened entity body
+    """
 
     # check if entity is an array
     if isinstance(entity, list):
@@ -64,6 +88,16 @@ def flatten_entity_body(entity, body, name = None):
     return body
 
 def format_entity_body(entity_body): 
+    """
+    Format the entity body
+
+    Args:
+        entity_body (dict): The entity body
+
+    Returns:
+        dict: The formatted entity body
+    """
+
     # Check if entity_body is an array
     if isinstance(entity_body, list):
         # Loop trought all the values
@@ -90,6 +124,17 @@ def format_entity_body(entity_body):
     return formatted_body
 
 async def create_entity(entity_name, body):
+    """
+    Create an entity
+
+    Args:
+        entity_name (str): The entity name
+        body (dict): The body
+
+    Raises:
+        PostgRESTError: If the entity cannot be created
+
+    """
 
     entity_body = {
         entity_name: body
@@ -150,6 +195,15 @@ async def create_entity(entity_name, body):
 
 
 def __flatten_navigation_links(row):
+    """
+    Flatten the navigation links
+
+    Args:
+        row (dict): The row
+
+    Returns:
+        dict: The flattened navigation links
+    """
     if row and "@iot.navigationLink" in row:
         # merge all the keys from the navigationLink
         row.update(row["@iot.navigationLink"])
@@ -160,6 +214,16 @@ def __flatten_navigation_links(row):
             del row["skip@iot.navigationLink"]
 
 def __flatten_expand_entity(data):
+    """
+    Flatten the entity
+
+    Args:
+        data (dict): The data
+
+    Returns:
+        dict: The flattened entity
+    """
+
     # Check if it is an array
     if not isinstance(data, list):
         # throw an error
@@ -178,6 +242,15 @@ def __flatten_expand_entity(data):
     return data
 
 def __create_ref_format(data):
+    """
+    Create the ref format
+
+    Args:
+        data (dict): The data
+
+    Returns:
+        dict: The ref format
+    """
 
     rows = [data]
 
